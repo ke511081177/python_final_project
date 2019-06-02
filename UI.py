da7829cb7480db2f164611f4dbc4f9cf4c9fe3c4
ke511081177 <ke511081177@gmail.com>
2019-06-02 18:57:50 +0800

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,15 +1,5 @@
 
 
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun  1 11:57:06 2019
-
-@author: Administrator
-"""
-
-
-
-
 
 import sys, os, random
 from PyQt5 import QtCore, QtWidgets, QtGui
@@ -20,23 +10,54 @@ from PyQt5.QtCore import Qt, QMimeData
 from PyQt5.QtGui import QIcon, QPixmap
 
 
-
-def ine():
-    print("dfdfdfdffd")
-        
-
+actions = []
 
 class initialization_Button(QPushButton):
-
     
     def __init__(self, title, parent=None):
         super().__init__(title, parent)
         self.setIcon(QIcon('icon.jpg'))
         self.setIconSize(QSize(200,200))
-        self.clicked.connect(AppForm().add_Photo)
+        self.clicked.connect(self.add_Photo)
             
         
+    def add_Photo(self):
+
+        global fileName_choose
         
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        
+        fileName_choose, filetype = QFileDialog.getSaveFileName(self,
+                                    "新增圖片")
+
+        f = open(fileName_choose,'r')
+        self.add_button()
+        
+    def add_button(self):
+        global photo_column
+        global photo_row
+        
+        photo_row +=1
+        
+        if photo_row == 3:
+            photo_row =0
+            photo_column+=1
+        
+        print(photo_column)
+        print(photo_row)
+            
+        self.button = Photo_Button('')
+        
+        self.button.setGeometry(50, 40, 200, 200)
+        
+        layout.addWidget(self.button,photo_column,photo_row)
+        
+        if photo_row == 2:
+            layout.addWidget(initialize_button,photo_column+1,0)
+        else:
+            layout.addWidget(initialize_button,photo_column,photo_row+1)
+        
+
 
 
 
@@ -45,7 +66,7 @@ fileName_choose = ''
 
 
 
-photo_space = QGroupBox("Grid layout")
+photo_space = QGroupBox("請輸入你要警示對象的圖片")
 
 layout = QGridLayout()
 photo_space.setMinimumSize(600,400)
@@ -116,47 +137,96 @@ class AppForm(QMainWindow):
     def on_about(self):
         msg ="""
         
-        加油。
+        有一天有一個小朋友到了河邊玩
+
+        傍晚卻還沒回家
+
+        小孩的家人都很著急的想找他
+
+        卻一直都找不到
+
+        過了三年，大家都放棄了
+
+        唯有奶奶每天還是會去河邊看看有沒有孩子的蹤影
+
+        而這份感情也感動了湖中女神
+
+        於是湖中女神決定幫助阿嬤
+
+        湖中女神說：阿嬤，你掉了什麼嗎？
+
+        阿嬤激動地說：我的金孫不見了，你有看到他嗎？
+
+        湖中女神說：哎呀，你太貪心了唷。（接著離去。
         """
         QMessageBox.about(self, "簡介", msg.strip())
-    
+    def a(self,name):
+        print(name)
+        
     def create_main_frame(self):
+        
+        global actions
+        
         self.main_frame = QWidget()
-
-
 
 
 
 
         finalbox = QVBoxLayout()
 
-# =============================================================================
-#         self.view = View(self)
-# =============================================================================
 
 
-
-
+        toolbarBox = QToolBar('開啟程式~~~~~~~~~',self)
+        toolbarBox.setFixedWidth(180)
+        
+        self.addToolBar(QtCore.Qt.RightToolBarArea, toolbarBox)
+        
+        vscode_action = toolbarBox.addAction('VSCode')
+        ptt_action = toolbarBox.addAction('Ppt')
+        word_action = toolbarBox.addAction('Word')
+        excel_action = toolbarBox.addAction('Excel')
+        other_action = toolbarBox.addAction('其他程式')
+        
+        
+        i = 0
+        for w in [vscode_action,ptt_action,word_action,
+                  excel_action,other_action]:
+            i+=1    
+            w.setCheckable(True)
+            w.name = i
+            actions.append(w.sender)
+        
+        for w in [vscode_action,ptt_action,word_action,
+                  excel_action,other_action]:
+            w.triggered.connect(self.toolbar_check)
+            
+ 
 
 
         self.cb = QCheckBox('警告訊息')
+        self.cb.stateChanged.connect(self.cb_check)
         self.warning_word =QTextEdit()
         self.warning_word.setMaximumSize(400,40)
+        self.warning_word.setReadOnly(True)
+        self.warning_word.setStyleSheet("background-color: gray")
         
-        self.choose_Button = QPushButton("選擇")   
+        self.choose_Button = QPushButton("選擇")  
+        self.choose_label = QLabel('開啟程式')
         self.choose_Button.setCheckable(True)
         self.choose_Button.setMaximumSize(60,30)
         
-        self.button =  QPushButton("choose") 
+
+
+        
+
         upper_grid = QGridLayout()
        
         upper_grid.addWidget(photo_space,0,0,6,6)
         upper_grid.addWidget(self.cb,1,7)
         upper_grid.addWidget(self.warning_word,2,7)
-        upper_grid.addWidget(self.choose_Button,4,7)
-        upper_grid.addWidget(self.button,5,7)
-        self.button.clicked.connect(self.add_button)
-        
+        upper_grid.addWidget(self.choose_label,3,7)
+        upper_grid.addWidget(toolbarBox,5,7)
+
         
 
 
@@ -184,58 +254,68 @@ class AppForm(QMainWindow):
         self.main_frame.setLayout(finalbox)
         self.setCentralWidget(self.main_frame)
     
+    def toolbar_check(self,a):
+        global actions
+        
+        print(self.sender())
+        print(self.sender().isChecked())
+        if self.sender().isChecked():
+            for w in actions:
+                if w() != self.sender():
+                    w().setCheckable(False)
+                    
+        if self.sender().isChecked():
+            for w in actions:
+                w().setCheckable(True)
+
+# =============================================================================
+#         if self.sender().isChecked():
+# =============================================================================
+            
+
+# =============================================================================
+#         if self.isChecked():
+#             for w in actions:
+#                 if self.name != w:
+#                     setCheckable(False)
+# =============================================================================
+# =============================================================================
+#         if self..isChecked():
+#             self.warning_word.setReadOnly(False)
+#           
+#             
+#         else:
+#             self.warning_word.setReadOnly(True)
+# =============================================================================
+          
+    
+    def cb_check(self,a):
+        
+        print(a)
+        if self.cb.isChecked():
+            self.warning_word.setReadOnly(False)
+            self.warning_word.setStyleSheet("background-color: white")
+            
+        else:
+            self.warning_word.setReadOnly(True)
+            self.warning_word.setStyleSheet("background-color: gray")
+
+    
     def other_file(self):
     
         fileName_choose, filetype = QFileDialog.getSaveFileName(self,
-                                    "")
+                                    "其他程式")
 
         f = open(fileName_choose,'r')
         
         
-    def add_Photo(self):
 
-        global fileName_choose
-        
-       
-        
-        fileName_choose, filetype = QFileDialog.getSaveFileName(self,
-                                    "新增圖片")
-
-        f = open(fileName_choose,'r')
-        self.add_button()
 
     def create_menu(self):        
-        menu = self.menuBar().addMenu('檔案')
-        new_action = menu.addAction('創建新檔')
-        new_action.triggered.connect(self.add_Photo)
-
-        
+     
         about_action = self.menuBar().addAction('介紹')
         about_action.triggered.connect(self.on_about)
     
-    def add_button(self):
-        global photo_column
-        global photo_row
-        
-        photo_row +=1
-        
-        if photo_row == 3:
-            photo_row =0
-            photo_column+=1
-        
-        print(photo_column)
-        print(photo_row)
-            
-        self.button = Photo_Button('')
-        
-        self.button.setGeometry(50, 40, 200, 200)
-        
-        layout.addWidget(self.button,photo_column,photo_row)
-
-
-# =============================================================================
-#         self.button.show() 
-# =============================================================================
 
 def main():
     app = QApplication(sys.argv)
